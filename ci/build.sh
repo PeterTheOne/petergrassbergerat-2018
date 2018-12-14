d031275e6555bf9d762080d0eb2ca4e8098236ea
Peter Grassberger <petertheone@gmail.com>
2018-12-14 17:43:49 +0100

    travis-ci htmlproofer ignore uploads and rss folders

--- ci/build.sh
+++ ci/build.sh
@@ -3,5 +3,5 @@ set -e # halt script on error
 
 bundle exec jekyll build --config _config.yml,_config_de.yml
 bundle exec jekyll build --config _config.yml,_config_en.yml
-bundle exec htmlproofer ./_site/de --disable-external
-bundle exec htmlproofer ./_site/en --disable-external
+bundle exec htmlproofer ./_site/de --disable-external --url_ignore "/\/uploads\/.*/,/\/rss\//"
+bundle exec htmlproofer ./_site/en --disable-external --url_ignore "/\/uploads\/.*/,/\/rss\//"
